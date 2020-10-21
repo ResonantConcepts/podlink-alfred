f946285ce429ee2935b7a6e2ec08f369b96ed194
Nathan Gathright <nathan.gathright@gmail.com>
2020-10-21 10:48:12 -0500

    Add 1hr cache

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const https = require('https');
 const alfy = require('alfy');
 
 let media = path.join(__dirname, 'media');
+let cachelength = 60 * 60 * 1000; // 1 hour cache
 
 function ensureDirectoryExistence(filePath) {
 	var dirname = path.dirname(filePath);
@@ -30,7 +31,7 @@ function download(filename, url, callback) {
 }
 
 (async () => {
-	const data = await alfy.fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(alfy.input)}&limit=20&media=podcast`);
+	const data = await alfy.fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(alfy.input)}&limit=20&media=podcast`, {maxAge: cachelength});
 
 	const results = data.results.map(result => {
 		const trackCount = (result.trackCount === 300) ? '300+' : result.trackCount;
